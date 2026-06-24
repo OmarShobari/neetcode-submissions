1fb25c29d9dff48cebd8724bf1211cf90ae8fac5
OmarShobari <124106448+OmarShobari@users.noreply.github.com>
2026-06-24 13:21:14 +0300

    Delete Data Structures & Algorithms directory

--- Data Structures & Algorithms/duplicate-integer/submission-3.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean hasDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-         set.add(nums[i]);
-        }
-        return nums.length != set.size();
-    }
-}
